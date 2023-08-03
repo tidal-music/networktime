afc6c55cd599fd6776e7cae15a9ea73b3e6306de
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-08-03 22:52:54 +0200

    Drop support for anycast NTP servers

--- library/src/commonMain/kotlin/com/tidal/networktime/NTPServer.kt
+++ library/src/commonMain/kotlin/com/tidal/networktime/NTPServer.kt
@@ -3,40 +3,39 @@ package com.tidal.networktime
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-sealed interface NTPServer {
-  val hostName: String
-  val responseTimeout: Duration
-  val dnsResolutionStrategy: DNSResolutionStrategy
-
-  sealed interface Unicast : NTPServer {
-    class Sequential(
-      override val hostName: String,
-      override val responseTimeout: Duration = RESPONSE_TIMEOUT_DEFAULT_SECONDS.seconds,
-      override val dnsResolutionStrategy: DNSResolutionStrategy = DNSResolutionStrategy.ALL,
-      val outgoingRequestGap: Duration = OUTGOING_REQUEST_GAP_DEFAULT_SECONDS.seconds,
-    ) : Unicast
-
-    class Concurrent(
-      override val hostName: String,
-      override val responseTimeout: Duration = RESPONSE_TIMEOUT_DEFAULT_SECONDS.seconds,
-      override val dnsResolutionStrategy: DNSResolutionStrategy = DNSResolutionStrategy.ALL,
-    ) : Unicast
-
-    companion object {
-      private const val RESPONSE_TIMEOUT_DEFAULT_SECONDS = 5
-      private const val OUTGOING_REQUEST_GAP_DEFAULT_SECONDS = 2
-    }
-  }
-
-  class Anycast(
-    override val hostName: String,
-    override val responseTimeout: Duration = 68.seconds,
-    override val dnsResolutionStrategy: DNSResolutionStrategy = DNSResolutionStrategy.ALL,
-  ) : NTPServer
+/**
+ * Describes a host name that can resolve to any number of NTP unicast servers.
+ *
+ * @param hostName The host name.
+ * @param responseTimeout The timeout for receiving responses from servers resolved from [hostName].
+ * @param dnsResolutionStrategy Can be used for filtering resolved address on [hostName] based on
+ * IP version.
+ * @param perAddressRequestGap The amount of time to wait before consecutive requests to the same
+ * resolved address.
+ * @param pinnedPortNumber The port number to send packets on. If null, a random choice of port
+ * be made for every packet, as recommended by RFC 9109.
+ * @param ntpVersion The version number to write in packets.
+ */
+class NTPServer(
+  val hostName: String,
+  val responseTimeout: Duration = 5.seconds,
+  val dnsResolutionStrategy: DNSResolutionStrategy = DNSResolutionStrategy.ALL,
+  val perAddressRequestGap: Duration = 2.seconds,
+  val pinnedPortNumber: Int? = null,
+  val ntpVersion: NTPVersion = NTPVersion.FOUR,
+) {
 
   enum class DNSResolutionStrategy {
     IP_V4,
     IP_V6,
     ALL,
+  }
+
+  enum class NTPVersion(val descriptor: Short) {
+    ZERO(0),
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
   }
 }
--- library/src/commonMain/kotlin/com/tidal/networktime/SNTPClient.kt
+++ library/src/commonMain/kotlin/com/tidal/networktime/SNTPClient.kt
@@ -2,30 +2,20 @@ package com.tidal.networktime
 
 import kotlinx.coroutines.CoroutineScope
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 /**
  * Construct a new SNTP client that can be requested to periodically interact with the provided
  * [ntpServers] to obtain information about their provided time.
  *
- * @param ntpServers Representation of supported NTP sources.
+ * @param ntpServers Representation of supported unicast NTP sources.
  * @param coroutineScope The scope where synchronization will run on.
- * @param ntpVersion The version number to write in packets.
- * @param portSelectionStrategy The strategy for selecting a port to operate on.
- * @param minimumSynchronizationInterval The minimum amount of time between performing time queries
- * on all unicast sources. The actual value used may be larger than this on occasion based on
- * changes to the difference between the time provided by [ntpServers] and [referenceClock] (if it
- * ever changes), but will never be lower than this value.
- * @param referenceClock A clock used to calculate timing differences with the information obtained
- * from [ntpServers]. This clock will never be modified directly.
+ * @param referenceClock A provider of UNIX time, used to calculate timing differences with the
+ * information obtained from [ntpServers].
  */
 class SNTPClient(
   vararg val ntpServers: NTPServer,
   val coroutineScope: CoroutineScope,
-  val ntpVersion: NTPVersion = NTPVersion.FOUR,
-  val portSelectionStrategy: PortSelectionStrategy = PortSelectionStrategy.RFC9109,
-  val minimumSynchronizationInterval: Duration = 64.seconds,
-  val referenceClock: () -> Long,
+  val referenceClock: () -> Duration,
 ) {
 
   val time: Long?
@@ -34,21 +24,4 @@ class SNTPClient(
   fun startSynchronization(): Unit = TODO("Start or return")
 
   fun stopSynchronization(): Unit = TODO("Stop or return")
-
-  enum class NTPVersion(val descriptor: Short) {
-    ZERO(0),
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-  }
-
-  sealed class PortSelectionStrategy(val pinnedPortNumber: Int?) {
-    data object RFC5905 : PortSelectionStrategy(123)
-
-    /**
-     * Make a new selection of a random available port every time a socket is opened.
-     */
-    data object RFC9109 : PortSelectionStrategy(null)
-  }
 }

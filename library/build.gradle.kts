33bdeaa714d78bfc3c06ef3b40d73eec05816e87
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-08-03 17:46:11 +0200

    Add all targets
    Some targets impose constraints on how things can be done so it's good to have
    them from the beginning.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   kotlin("multiplatform")
+  id("com.android.library")
 }
 
 group = "com.tidal.network-time"
@@ -8,4 +9,26 @@ kotlin {
   targetHierarchy.default()
 
   jvm()
+  androidTarget()
+  ios()
+  linuxX64()
+  macosX64()
+  macosArm64()
+  iosSimulatorArm64()
+  iosX64()
+  linuxArm64()
+  watchosSimulatorArm64()
+  watchosX64()
+  watchosArm32()
+  watchosArm64()
+  tvosSimulatorArm64()
+  tvosX64()
+  tvosArm64()
+  iosArm64()
+  androidNativeArm32()
+  androidNativeArm64()
+  androidNativeX86()
+  androidNativeX64()
+  mingwX64()
+  watchosDeviceArm64()
 }

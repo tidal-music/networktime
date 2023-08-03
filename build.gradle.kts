d88600cf2d6c2b98ee0e249a8618693ad794f7bf
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-08-03 17:46:11 +0200

    Add all targets
    Some targets impose constraints on how things can be done so it's good to have
    them from the beginning.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 buildscript {
   repositories {
     gradlePluginPortal()
+    google()
   }
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    classpath("com.android.tools.build:gradle:8.0.2")
   }
 }
 

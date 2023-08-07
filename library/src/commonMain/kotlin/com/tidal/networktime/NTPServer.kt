2041b9130700dd3ca89b2b75f536421e4220e50d
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-08-07 09:18:15 +0200

    Add NTPServer.queriesPerResolvedAddress

--- library/src/commonMain/kotlin/com/tidal/networktime/NTPServer.kt
+++ library/src/commonMain/kotlin/com/tidal/networktime/NTPServer.kt
@@ -10,8 +10,11 @@ import kotlin.time.Duration.Companion.seconds
  * @param responseTimeout The timeout for receiving responses from servers resolved from [hostName].
  * @param dnsResolutionStrategy Can be used for filtering resolved address on [hostName] based on
  * IP version.
- * @param perAddressRequestGap The amount of time to wait before consecutive requests to the same
- * resolved address.
+ * @param queriesPerResolvedAddress The amount of queries to perform to each resolved address. More
+ * queries may or may not increase precision, but they will make synchronization take longer and
+ * also cause more server load.
+ * @param waitBetweenResolvedAddressQueries The amount of time to wait before consecutive requests
+ * to the same resolved address.
  * @param pinnedPortNumber The port number to send packets on. If null, a random choice of port
  * be made for every packet, as recommended by RFC 9109.
  * @param ntpVersion The version number to write in packets.
@@ -20,7 +23,8 @@ class NTPServer(
   val hostName: String,
   val responseTimeout: Duration = 5.seconds,
   val dnsResolutionStrategy: DNSResolutionStrategy = DNSResolutionStrategy.ALL,
-  val perAddressRequestGap: Duration = 2.seconds,
+  val queriesPerResolvedAddress: Short = 3,
+  val waitBetweenResolvedAddressQueries: Duration = 2.seconds,
   val pinnedPortNumber: Int? = null,
   val ntpVersion: NTPVersion = NTPVersion.FOUR,
 ) {

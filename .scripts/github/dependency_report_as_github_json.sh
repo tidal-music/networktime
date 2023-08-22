138e6661aba8a8757c7ca4939c9646b114e4488a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-08-22 15:44:44 +0200

    Report dependency file location

--- .scripts/github/dependency_report_as_github_json.sh
+++ .scripts/github/dependency_report_as_github_json.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -e
 
-DIR_TMP="build/report_dependencies_github_json"
-rm -rf $DIR_TMP || true
-mkdir -p $DIR_TMP
-
 print_usage()
 {
-  echo "Usage: $0 -i <input_file> -n <manifest_name> -s <scanned_at>"
+  echo "Usage: $0 -i <input_file> -n <manifest_name> -s <scanned_at> -l <source_location>"
 }
 
-while getopts ":i:n:s:" OPT; do
+while getopts ":i:n:s:l:" OPT; do
   case $OPT in
     i) INPUT_FILE="$OPTARG"
     ;;
     n) MANIFEST_NAME="$OPTARG"
     ;;
     s) SCANNED_AT="$OPTARG"
+    ;;
+    l) SOURCE_LOCATION="$OPTARG"
     ;;
     ?) print_usage
        exit 1
@@ -35,6 +33,10 @@ if [ -z "${SCANNED_AT+x}" ]; then
   print_usage
   exit 1
 fi
+if [ -z "${SOURCE_LOCATION+x}" ]; then
+  print_usage
+  exit 1
+fi
 
 JSON=$(jq --null-input \
 --argjson VERSION 0 \
@@ -44,7 +46,7 @@ JSON=$(jq --null-input \
 --arg RUN_ID "$GITHUB_RUN_ID" \
 --arg HTML_URL "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" \
 --arg DETECTOR_NAME "$GITHUB_REPOSITORY" \
---arg DETECTOR_VERSION 0 \
+--arg DETECTOR_VERSION 1 \
 --arg DETECTOR_URL "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
 --arg SCANNED "$SCANNED_AT" \
 --arg MANIFEST_NAME "$MANIFEST_NAME" \
@@ -67,6 +69,9 @@ JSON=$(jq --null-input \
   "manifests":{
     ($MANIFEST_NAME):{
       "name":$MANIFEST_NAME,
+      "file": {
+        "source_location":$SOURCE_LOCATION
+      },
       "resolved":{
       }
     }

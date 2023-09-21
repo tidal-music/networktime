e81fccf177b73c92a17c715eacae06c9c1ffa1e2
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-09-21 10:22:25 +0200

    Add JVM sample module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
 rootProject.name = "network-time"
 
 include("library")
+file("samples").apply {
+  list { file, _ -> file.isDirectory }!!
+    .forEach {
+      include(":$it")
+      project(":$it").projectDir = toPath().resolve(it).toFile()
+    }
+}

005abeb2593394f2553a85f40833337a99dc44af
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-10-17 19:28:46 +0200

    Sync with each server in parallel

--- library/src/commonMain/kotlin/com/tidal/networktime/internal/SyncSingular.kt
+++ library/src/commonMain/kotlin/com/tidal/networktime/internal/SyncSingular.kt
@@ -2,23 +2,33 @@ package com.tidal.networktime.internal
 
 import com.tidal.networktime.DnsLookupStrategy
 import com.tidal.networktime.NTPServer
+import kotlinx.coroutines.async
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.withContext
 
 internal class SyncSingular(
   private val domainNameResolver: DomainNameResolver,
   private val ntpServers: Iterable<NTPServer>,
 ) {
   suspend operator fun invoke() {
-    // TODO Make iterations of this forEach parallel with each other
-    ntpServers.forEach { ntpServer ->
-      domainNameResolver(
-        ntpServer.name,
-        when (ntpServer.dnsLookupStrategy) {
-          DnsLookupStrategy.IP_V4 -> listOf(DnsResourceRecord.A)
-          DnsLookupStrategy.IP_V6 -> listOf(DnsResourceRecord.AAAA)
-          DnsLookupStrategy.ALL -> listOf(DnsResourceRecord.A, DnsResourceRecord.AAAA)
-        },
-      )
-      // TODO Take the addresses and continue with the protocol
+    ntpServers.map {
+      withContext(currentCoroutineContext()) {
+        async { sync(it) }
+      }
+    }.onEach {
+      it.await()
     }
+  }
+
+  private suspend fun sync(ntpServer: NTPServer) = with(ntpServer) {
+    domainNameResolver(
+      name,
+      when (dnsLookupStrategy) {
+        DnsLookupStrategy.IP_V4 -> listOf(DnsResourceRecord.A)
+        DnsLookupStrategy.IP_V6 -> listOf(DnsResourceRecord.AAAA)
+        DnsLookupStrategy.ALL -> listOf(DnsResourceRecord.A, DnsResourceRecord.AAAA)
+      },
+    )
+    // TODO Take the addresses and continue with the protocol
   }
 }

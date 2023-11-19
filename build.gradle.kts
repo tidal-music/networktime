ccf830b5f8d0471198d24078176aef816239b926
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-11-19 10:15:29 +0100

    Bump AGP to 8.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     val kotlinVersion = "1.9.20"
     classpath(kotlin("gradle-plugin", version = kotlinVersion))
     classpath(kotlin("serialization", version = kotlinVersion))
-    classpath("com.android.tools.build:gradle:8.1.2")
+    classpath("com.android.tools.build:gradle:8.1.4")
   }
 }
 

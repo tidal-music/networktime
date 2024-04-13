3847cb2509c4b1389be783c818bc7a7a55df3015
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2024-04-13 20:15:09 +0200

    More script fixes

--- .scripts/github/release.sh
+++ .scripts/github/release.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
+set -e
+
 BODY="{
   \"tag_name\": \"${GITHUB_REF_NAME}\",
   \"target_commitish\": \"${GITHUB_SHA}\",
   \"name\": \"${GITHUB_REF_NAME}\",
-  \"body\": \"$(cat ../../changelog/"$GITHUB_REF_NAME")\"
+  \"body\": \"$(cat "$GITHUB_WORKSPACE"/changelog/"$GITHUB_REF_NAME")\"
 }"
 
-RESPONSE_BODY=$(curl -s \
-  -u "${GITHUB_ACTOR}":"${GITHUB_TOKEN}" \
-  --header "Accept: application/vnd.github.v3+json" \
-  --header "Content-Type: application/json; charset=utf-8" \
+RESPONSE_BODY=$(curl -sL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   --request POST \
   --data "${BODY}" \
   https://api.github.com/repos/"${GITHUB_REPOSITORY}"/releases)
